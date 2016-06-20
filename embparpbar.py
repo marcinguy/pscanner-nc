f2147ed8344637972ba04722cf1644cf39553591
Marcin Kozlowski <marcinguy@gmail.com>
2016-06-20 16:32:51 +0200

    Initial commit

---
+++ embparpbar.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" A multiprocess `Pool` variant with a `ProgressBar` and ncurses/blessings """
+
+
+import time
+from multiprocessing.pool import Pool
+from progressbar import ProgressBar, Percentage, Bar, ETA
+
+from blessings import Terminal
+
+term = Terminal()
+
+location = (0, 20)
+
+
+
+class Writer(object):
+    """Create an object with a write method that writes to a
+    specific place on the screen, defined at instantiation.
+    This is the glue between blessings and progressbar.
+    """
+    def __init__(self, location, term):
+        """
+        Input: location - tuple of ints (x, y), the position
+                          of the bar in the terminal
+        """
+        self.location = location
+        self.term = term
+
+    def write(self, string):
+        with term.location(*self.location):
+            print(string)
+
+
+
+class ProgressPool(Pool):
+    """ Extension of `multiprocessing.Pool` with `ProgressBar`.
+
+    The `map` function now displays a progress bar. The usual caveats about
+    multiprocessing not working in an interactive interpreter apply.
+
+    """
+
+    def map(self, func, iterable, chunksize=1, pbar='ProgressPool'):
+        """ Apply function on iterables in available subprocess workers.
+
+        Parameters
+        ----------
+        func : callable
+            the function to execute
+        iterable : iterable
+            the arguments to the func
+        chunksize : int, default: 1
+            the approximate number of tasks to distribute to a process at once
+        pbar : str or ProgressBar
+            if str, use the string in a standard ProgressBar, else use the
+            given ProgressBar
+
+        Returns
+        -------
+        results : list
+            the result of applying func to each value in iterables
+
+        Raises
+        ------
+        TypeError if the pbar argument has the wrong type
+
+        """
+        # need to get the length, for the progress bar
+        if not hasattr(iterable, '__len__'):
+            iterable = list(iterable)
+        total_items = len(iterable)
+
+        # initialize the progress bar
+        if isinstance(pbar, str):
+            writer = Writer(location,term)
+            pbar = ProgressBar(widgets=['%s: ' % pbar,
+                Percentage(), Bar(), ETA()],
+                maxval=total_items,fd=writer).start()
+        elif isinstance(pbar, ProgressBar):
+            pass
+        else:
+            raise TypeError("pbar must be of type 'string' or, "+\
+                    "'ProgressBar' you gave: "+\
+                    "'%s' of type %s" % (pbar, type(pbar)))
+
+        # get the pool working asynchronously
+        a_map = self.map_async(func, iterable, chunksize)
+
+        # Crux: monitor the _number_left of the a_map, and update the progress
+        # bar accordingly
+        # TODO should probably check for termination on each run here
+        while True:
+            time.sleep(0.1)
+            left = a_map._number_left
+            pbar.update(total_items-left)
+            if left == 0:
+                break
+        pbar.finish()
+        return a_map.get()
+
---
+++ pscanner.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Parallel IP scanner with progressbar and SSL support  """
+
+__author__ = "Marcin Kozlowski <marcinguy@gmail.com>"
+
+import argparse
+
+import multiprocessing
+
+import ssl
+import socket
+from socket import gethostbyname, gaierror
+
+from dns import reversename, resolver, exception
+import sys
+
+import M2Crypto
+from embparpbar import ProgressPool
+
+
+from blessings import Terminal
+
+import pprint
+
+location = (0, 10)
+
+term = Terminal()
+
+
+
+
+def scan(d):
+        with term.location(*location):
+          print term.bold_red_on_bright_green("Scanning: "+d)
+        
+        if(sslp=="yes"): 
+          s_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	  s = ssl.wrap_socket(s_, ca_certs='/usr/local/lib/python2.7/dist-packages/requests/cacert.pem',cert_reqs=ssl.CERT_OPTIONAL)
+ 
+          s.settimeout(0.1)
+          d=str(d) 
+          try:
+            result = s.connect_ex((d, int(port)))
+          except Exception, e:
+                message = "Error: "+d.rstrip()+","+getrev(d)
+                message += str(e)
+                try: 
+                  cert = ssl.get_server_certificate((d, 443), ssl_version=ssl.PROTOCOL_TLSv1)
+                  x509 = M2Crypto.X509.load_cert_string(cert)
+                  r = x509.get_subject().as_text()
+                  val = r.split(",")
+                  for i, j in enumerate(val):
+                    if j.find("CN=") != -1:
+                      val[i]=j.replace("CN=","")
+                      val[i]=val[i].strip()
+                
+                  message += ","+val[i]
+                  return message
+                except Exception, e:
+                       return d.rstrip()+","+getrev(d)+","+"CERT ERROR!"
+          except ssl.SSLError:
+            return d.rstrip()+","+getrev(d)+","+"CERT ERROR!"
+          except socket.gaierror:
+            return d.rstrip()+","+"SOCKET ERROR!"
+          except socket.error:
+            return d.rstrip()+","+"SOCKET ERROR!"
+          if result:
+            return "No SSL"
+          else:
+            cert = s.getpeercert()
+            if cert:
+              subject = dict(x[0] for x in cert['subject'])
+              issued_to = subject['commonName']
+            else:
+              subject = "None"
+              issued_to = "None"
+            return d.rstrip()+","+getrev(d)+","+"CN:"+issued_to+","+"CERT OK"
+ 
+        if(sslp=="no"):
+          d=str(d)
+          try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.1)
+            result = s.connect_ex((d, int(port)))
+            s.close()
+          except Exception, e:
+            message = "Error: "+d.rstrip()+","+getrev(d)
+            message += str(e)
+            return message
+   
+          if result:
+            return d.rstrip()+","+port+",closed"
+          else:
+            return d.rstrip()+","+port+",open"
+ 
+
+
+def getrev(ip):
+
+      try:
+        rev_name = reversename.from_address(str(ip.rstrip()))
+        resolverobj = resolver.Resolver()
+        resolverobj.timeout = 1
+        resolverobj.lifetime = 1
+        reversed_dns = str(resolverobj.query(rev_name,"PTR")[0])
+        reversed_dns = reversed_dns[:-1]
+        return reversed_dns
+      except resolver.NXDOMAIN:
+        return "unknown"
+      except resolver.Timeout:
+        return "Timed out while resolving %s" % ip.rstrip()
+      except exception.DNSException:
+        return "Unhandled exception"
+
+
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='Port Scanner v0.99')
+        parser.add_argument('-i','--input', help='Input list of IPs', required=True)
+        parser.add_argument('-o','--output', help='Output', required=True)
+        parser.add_argument('-p','--port', help='Port number(s)', required=True)
+        parser.add_argument('-s','--ssl', help='SSL yes|no', required=True)
+        args = parser.parse_args()
+        
+        input = args.input 
+        output = args.output
+        port = args.port
+        sslp = args.ssl
+        data = open(input,'rU')
+	# Create pool (ppool)
+        ppool = ProgressPool()
+
+        with term.fullscreen():
+
+          results = ppool.map(scan, data, pbar="Scanning")
+          resfile = open(output,'w') 
+	  for r in results:
+            resfile.write(r+"\n") 
