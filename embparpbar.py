f99652c344934190e34e271ab892ee92253623bd
Marcin Kozlowski <marcinguy@gmail.com>
2016-06-27 13:53:31 +0200

    Author

--- embparpbar.py
+++ embparpbar.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__author__ = "Marcin Kozlowski <marcinguy@gmail.com>"
+
 """ A multiprocess `Pool` variant with a `ProgressBar` and ncurses/blessings """
 
 
